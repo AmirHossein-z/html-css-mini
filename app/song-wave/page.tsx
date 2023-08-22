b00806865c9e797aa2b3b4c3a9d0de0e42dbf6e1
Amirhossein Zareian <amirhosseinzareian01@gmail.com>
2023-08-22 23:54:29 +0330

    add song-wave & move fonts to public folder

---
+++ app/song-wave/page.tsx
@@ -0,0 +1,16 @@
+import styles from "./styles.module.css";
+
+export default function WaveSong() {
+  return (
+    <section className={styles.wave}>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+    </section>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      boxShadow: {
+        glass: "inset 0px 2px 22px 0px rgba(255,255,255,0.6)",
+      },
+      animation: {
+        fade_in_from_right: "fade_in_from_right 700ms 1",
+        fade_in_from_bottom: "fade_in_from_bottom 700ms 1",
+      },
+      keyframes: {
+        fade_in_from_right: {
+          "0%": { opacity: "0", transform: "translateX(128px)" },
+          "100%": { opacity: "1", transform: "translateX(0px)" },
+        },
+        fade_in_from_bottom: {
+          "0%": { opacity: "0", transform: "translateY(6px)" },
+          "100%": { opacity: "1", transform: "translateY(0px)" },
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
